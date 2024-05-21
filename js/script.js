5631804b416af2be4e22c5c7a0b1e786b3ce9955
류솔아 <mav7937@naver.com>
2024-05-21 11:37:30 +0900

    필터 아코디언 적용

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 // DOM 구조가 파악 되면 실행
 $(function () {
   // 대상을 변수에 저장
+  // Main Common
+  // Header
   const $header = $('header');
   const $menu = $('.gnb > li');
   const $gnbDropdownWrap = $('.gnb-dropdown-wrap');
@@ -16,5 +18,19 @@ $(function () {
     $(this).find($gnbDropdownWrap).stop().slideUp(duration);
 
     $header.addClass('active');
+  });
+
+  // Sub
+  // Luggage Filter
+  const $filterTitle = $('.filter-item');
+  const $filterCon = $('.filter-inner');
+
+  $filterTitle.on('click', function () {
+    // console.log($faqQ, $(this));
+    // 클릭한 질문의 답변이 보여지게
+    $(this).find($filterCon).stop().slideToggle(duration);
+
+    $(this).toggleClass('active');
+    // 클릭한 놈한테 on클래스 부여
   });
 });
